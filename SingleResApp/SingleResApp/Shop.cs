75eac14d6b9b8f7c91dcb87130ca5ea0e1155e74
TeamPlayer99 <32104436+TeamPlayer99@users.noreply.github.com>
2023-03-04 00:41:17 +0200

    Implemeted Open - Close principle

---
+++ SingleResApp/SingleResApp/Shop.cs
@@ -0,0 +1,24 @@
+﻿namespace SingleResApp
+{
+    public class Shop : VendingMachine
+    {
+        public Shop()
+        {
+
+        }
+
+        public string Open()
+        {
+            var str = "Shop is opening up for business";
+
+            return str;
+        }
+
+        public string Close()
+        {
+            var str = "Shop is closing down";
+
+            return str;
+        }
+    }
+}
